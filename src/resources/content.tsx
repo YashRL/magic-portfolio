19bd9418c3d727949133c2ef506e3b33fa2e41a0
yash <yash@talenteam.com>
2025-12-29 16:33:17 +0530

    minor updates

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -239,7 +239,7 @@ const about: About = {
 const blog: Blog = {
   path: "/blog",
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "My Personal Knowledge base...",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
